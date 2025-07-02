8cf3f05eac61706c9e50bee7a22906cfd22e4a5f
Akshya <142394538+akshya-backend@users.noreply.github.com>
2025-07-02 18:17:15 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,18 +8,18 @@ import routes from './routes/index-route.js';
 import { errorHandler } from './middlewares/Global-error-handler.js';
 import { sessionMiddleware } from './middlewares/Session-middleware.js';
 import corsMiddleware from './middlewares/Cors-security.js';
-// import helmetMiddleware from './middlewares/Helmet-security.js';
-// import {  setupCors, setupHelmet } from './middlewares/authentication-middleware.js';
-// import { appRateLimiter } from './middlewares/rate-limiting-middleware.js';
-// import { sessionMiddleware } from './middlewares/express-session-middleware.js';
+import helmetMiddleware from './middlewares/Helmet-security.js';
+import {  setupCors, setupHelmet } from './middlewares/authentication-middleware.js';
+import { appRateLimiter } from './middlewares/rate-limiting-middleware.js';
+import { sessionMiddleware } from './middlewares/express-session-middleware.js';
 
 
 const app = express();
 
-// // Security middleware
-// app.use(helmetMiddleware());
-// app.use(appRateLimiter);
-// app.use(corsMiddleware);
+// Security middleware
+app.use(helmetMiddleware());
+app.use(appRateLimiter);
+app.use(corsMiddleware);
 
 
 app.use(express.json());
@@ -33,15 +33,6 @@ setupStaticFilesAndViews(app);
 // Define routes
 routes(app);
 
-// // Error handling middleware for 404
-// app.use((req, res, next) => {
-//     const error = new Error('Page Not Found');
-//     error.status = 404;
-//     error.description = 'The page you are looking for does not exist.';    
-//     next(error);
-// }); 
 
-// // // Global error handler middleware
-// app.use(errorHandler);
 
 export default app;
